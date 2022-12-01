e71d0005964e3942582e8e225edf3d2e20351d57
KrutoiProgram <Andrey23451dss2@gmail.com>
2022-12-01 14:17:14 +0500

    Compl_13_glv

--- app/src/main/java/com/example/crimeintent/CrimeRepository.kt
+++ app/src/main/java/com/example/crimeintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.example.crimeintent.database.CrimeDatabase
 import java.util.*
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 class CrimeRepository private
@@ -15,10 +16,21 @@ constructor(context: Context) {
             CrimeDatabase::class.java,
             DATABASE_NAME
         ).build()
+    private val crimeDao = database.crimeDao()
+    private val executor = Executors.newSingleThreadExecutor()
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
     fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+    fun updateCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.updateCrime(crime)
+        }
+    }
+    fun addCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.addCrime(crime)
+        }
+    }
 
-    private val crimeDao = database.crimeDao()
     companion object {
         private var INSTANCE: CrimeRepository? =
             null
--- app/src/main/java/com/example/crimeintent/MainActivity.kt
+++ app/src/main/java/com/example/crimeintent/MainActivity.kt
@@ -27,7 +27,8 @@ class MainActivity : AppCompatActivity() , CrimeListFragment.Callbacks {
     }
     override fun onCrimeSelected(crimeId: UUID)
     {
-        val fragment = CrimeFragment()
+        val fragment = CrimeFragment.newInstance(crimeId)
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container,
